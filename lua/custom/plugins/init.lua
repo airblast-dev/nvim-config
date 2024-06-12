0768768bbcf9321f6d14b308e18f427ca194d83e
Tayfun Bocek <tayfunbocek@live.ca>
2024-06-12 22:00:16 +0300

    add go plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -49,4 +49,17 @@ return {
     version = '^4',
     ft = { 'rust' },
   },
+  {
+    'ray-x/go.nvim',
+    dependencies = { -- optional packages
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('go').setup()
+    end,
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+  },
 }
