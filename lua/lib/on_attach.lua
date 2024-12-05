b2f46f1e60371d0a2ff16d1366771f324623541f
Tayfun Bocek <tayfunbocek@live.ca>
2024-12-05 22:17:35 +0300

    remove deprecated method call

--- lua/lib/on_attach.lua
+++ lua/lib/on_attach.lua
@@ -6,7 +6,7 @@ end
 
 local on_attach = function(client, bufnr)
 	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	vim.api.nvim_set_option_value('omnifunc', 'lua.vim.lsp.omnifunc', {['buf']= bufnr})
 
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
