e3916470e3fdbb6163043c10627d1971b207de91
Tayfun Bocek <tayfunbocek@live.ca>
2024-12-05 18:24:30 +0300

    remove auto formatting

--- lua/options.lua
+++ lua/options.lua
@@ -1,9 +1,2 @@
-vim.api.nvim_create_autocmd("BufWritePre", {
-	buffer = buffer,
-	callback = function()
-		vim.lsp.buf.format { async = false }
-	end
-})
-
 vim.g.autoformat = true
 vim.opt.number = true
