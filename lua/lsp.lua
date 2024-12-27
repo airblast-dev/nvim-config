724d7a32a29dea7ffb9df922512d8b74277d0fdf
Tayfun Bocek <tayfunbocek@live.ca>
2024-12-27 13:43:53 +0300

    add trunkls lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -5,3 +5,15 @@ vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Create an event handler for the FileType autocommand
+vim.api.nvim_create_autocmd('FileType', {
+	-- This handler will fire when the buffer's 'filetype' is "python"
+	pattern = 'html',
+	callback = function(args)
+		vim.lsp.start({
+			name = 'trunkls',
+			cmd = { 'trunkls' },
+		})
+	end,
+})
