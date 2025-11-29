ec09f3927ef602b4f6da571e5af0fa709c58fab2
Tayfun Bocek <tayfunbocek@live.ca>
2025-11-29 10:23:39 +0300

    update

--- init.lua
+++ init.lua
@@ -2,3 +2,8 @@ require("lsp")
 require("config.lazy")
 require("config.keymaps")
 require("config.opts")
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = { 'c', 'cpp', 'lua' },
+	callback = function() vim.treesitter.start() end,
+})
--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,10 +1,12 @@
 local lsps = {
 	'lua_ls',
-	'clangd',
 	'bacon_ls',
 	'omnisharp',
 	'ruff',
 	'cmake',
+	'denols',
+	"ts_query_ls",
+	"gdscript"
 }
 
 vim.lsp.enable(lsps)
